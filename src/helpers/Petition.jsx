7304f8422a92d4beb221ee88da3e0dc433766df6
WilmerDep <wilmer96jhernandez@gmail.com>
2023-11-17 14:47:39 -0400

    commit archivo petition anadido a global

---
+++ src/helpers/Petition.jsx
@@ -0,0 +1,31 @@
+export const Petition = async (url, method, dataSave = "") => {
+  let loading = true;
+
+  let options = {
+    method: "GET",
+  };
+
+  if (method == "GET" || method == "DELETE") {
+    options = {
+      method: method,
+    };
+  }
+  if (method == "POST" || method == "PUT") {
+    options = {
+      method: method,
+      body: JSON.stringify(dataSave),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  }
+
+  const petition = await fetch(url, options);
+
+  const datas = await petition.json();
+  loading = false;
+  return {
+    datas,
+    loading,
+  };
+};
