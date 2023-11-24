6c47d8f6a3c32b688d2a73fd38b4e9573cd56763
WilmerDep <Wilmer96jhernandez@gmail.com>
2023-11-24 15:26:33 -0400

    commit userForm actualizado

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -1,40 +1,55 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export const useForm = () => {
-    const [form, setForm] = useState(objetInit);
-    const serializeForm = (form);
+    // Estado del formulario
+    const [form, setForm] = useState({});
 
+    // Función para serializar los datos del formulario
+    const serializeForm = (form) => {
         const formData = new FormData(form);
-
         const objetComplete = {};
 
-        for(let [name, value] of formData){
+        for (let [name, value] of formData) {
             objetComplete[name] = value;
         }
-    return objetComplete;
 
-    const submited = (e) =>{
+        return objetComplete;
+    };
+
+    // Función que se ejecuta al enviar el formulario
+    const submited = (e) => {
         e.preventDefault();
 
+        // Serializa los datos del formulario
         let curso = serializeForm(e.target);
 
-        setForm(curso);
-
-        document.querySelector(".code").classList.add("sumited");
-    }
-
-    const changed = ({target}) =>{
-        const {name, value} = target;
-
-        setForm({
-            ...form,
-            [name]: value
+        // Actualiza el estado del formulario
+        setForm((prevForm) => {
+            const updatedForm = { ...prevForm, ...curso };
+            return updatedForm;
         });
-    }
 
+        // Agrega una clase al elemento con la clase "code"
+        document.querySelector(".code").classList.add("sumited");
+    };
+
+    // Función que se ejecuta al cambiar un campo del formulario
+    const changed = ({ target }) => {
+        const { name, value } = target;
+
+        // Actualiza el estado del formulario
+        setForm((prevForm) => {
+            return {
+                ...prevForm,
+                [name]: value
+            };
+        });
+    };
+
+    // Devuelve el estado actual del formulario y las funciones para manejar eventos
     return {
         form,
         submited,
         changed
-    }
-}
+    };
+};
